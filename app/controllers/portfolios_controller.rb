9402601b06cc736911193322829c662f38c7e86d
thanhhai45 <thanhhai82451495@gmail.com>
2018-06-15 00:44:35 +0700

    create new and update portfolio item

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,38 @@ class PortfoliosController < ApplicationController
     def index
         @portfolio_items = Portfolio.all
     end
+
+    def new
+        @portfolio_items = Portfolio.new
+    end
+
+    def create
+        @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+        respond_to do |format|
+            if @portfolio_items.save
+                format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+                format.json { render :show, status: :created, location: @portfolio_items }
+            else
+                format.html { render :new }
+                format.json { render json: @portfolio_items.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    def edit
+        @portfolio_items = Portfolio.find(params[:id])
+    end
+
+    def update
+        @portfolio_items = Portfolio.find(params[:id])
+        respond_to do |format|
+            if @portfolio_items.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+                format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+                format.json { render :show, status: :ok, location: @portfolio_items }
+            else
+                format.html { render :edit }
+                format.json { render json: @portfolio_items.errors, status: :unprocessable_entity }
+            end
+        end
+    end
 end
