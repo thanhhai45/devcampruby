4c100ff92bc49525d753d5c34d3cdc748936ba81
thanhhai45 <thanhhai82451495@gmail.com>
2018-06-30 17:55:01 +0700

    Set default variable

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,11 @@ class Portfolio < ApplicationRecord
     end
 
     scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on Rails") }
+
+    after_initialize :set_defaults
+
+    def set_defaults
+        self.main_image ||= "http://via.placeholder.com/600x200"
+        self.thumb_image ||= "http://via.placeholder.com/300x150"
+    end
 end
